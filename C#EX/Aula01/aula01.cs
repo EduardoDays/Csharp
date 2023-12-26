1a1187586cf1a58ea1fc53e360cdc8b761c8ac0e
Eduardo Oliveira Dias <135471414+EduardoDays@users.noreply.github.com>
2023-12-25 21:07:53 -0300

    Commit hello world C#

---
+++ C#EX/Aula01/aula01.cs
@@ -0,0 +1,7 @@
+using System;
+
+class Principal{
+    static void Main(){
+      Console.Write("Ola mundo..."); //usando Console.Write é possivel imprimir informações no executavel de um programa.
+    }
+}
