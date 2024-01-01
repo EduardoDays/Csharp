0411d5ded7f2edff922fb183fa7afed1d701891a
Eduardo Oliveira Dias <135471414+EduardoDays@users.noreply.github.com>
2024-01-01 20:47:57 -0300

    Adição de comentário

--- C#/Aula10/Aula10.cs
+++ C#/Aula10/Aula10.cs
@@ -1,4 +1,4 @@
-using System;
+using System; //neste exercicio é possivel ver como o uso de enumerador pode ser feito dentro de um código.
 class Aula10{
     enum DiasSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sabado};
     static void Main(){
